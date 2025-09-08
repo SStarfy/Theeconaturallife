dc86f3767f160f33e521b2b249973dd0d24cff6d
SStarfy <diego.chavarria207@gmail.com>
2025-09-07 21:12:27 -0700

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,6 @@
+<a href="/kitchen" className="px-3 hover:text-green-600">Home & Kitchen</a>
+<a href="/cleaning" className="px-3 hover:text-green-600">Cleaning Supplies</a>
+<a href="/personal-care" className="px-3 hover:text-green-600">Personal Care</a>
+<a href="/travel" className="px-3 hover:text-green-600">Travel</a>
+<a href="/family-kids" className="px-3 hover:text-green-600">Family & Kids</a>
+<a href="/office" className="px-3 hover:text-green-600">Office</a>
